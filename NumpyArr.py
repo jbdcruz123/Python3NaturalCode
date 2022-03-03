a296748ef04deee14bef03c694f076590605d6cc
Ron S. Medrano <83150978+jbdcruz123@users.noreply.github.com>
2022-03-03 16:09:25 +0800

    Add files via upload
    using np.resize to resize array

---
+++ NumpyArr.py
@@ -0,0 +1,21 @@
+
+import numpy as np
+A = np.array([[1,2], [3,4]])
+
+A = np.resize(A, (3,5))
+
+V =0
+I=0
+while I < 3: #2
+
+  J =0
+
+  while J < 5: #3
+
+    J +=1  
+  #3
+  
+  I +=1
+#2
+
+print(A)
